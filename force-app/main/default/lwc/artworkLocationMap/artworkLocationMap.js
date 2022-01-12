a43aed7e7b0e450a01c826ba62f45981d1981c21
Desiree Dixon <ddixon@crystalbridges.org>
2022-01-12 11:40:53 -0600

    Edited text on map markers

--- force-app/main/default/lwc/artworkLocationMap/artworkLocationMap.js
+++ force-app/main/default/lwc/artworkLocationMap/artworkLocationMap.js
@@ -23,32 +23,32 @@ export default class ArtworkLocationMap extends LightningElement {
             Latitude: '40.95361',
             Longitude: '-73.895789',
         }, 
-        title: "'Light Atlas' - Cynthia Daignault",
-        description: `<b>Hudson River Museum</b><br> Yonkers, NY.<br><br> On View: 8/12/2020 to 8/2/2022`,
+        title: "'Light Atlas' by Cynthia Daignault",
+        description: `<b>Hudson River Museum</b><br> Yonkers, NY.<br><br> On View until 8/2/2022`,
         icon: 'standard:brand'
         }, 
         {location: {
             Latitude: '27.774668',
             Longitude: '-82.631816',
         }, 
-        title: "'Untitled (Subway Station)' - Norman Lewis",
-        description: `<b>Museum of Fine Arts<br> St. Petersburg</b><br> St. Petersburg, FL <br><br> On View: 1/1/2021 to 6/15/2022`,
+        title: "'Untitled (Subway Station)' by Norman Lewis",
+        description: `<b>Museum of Fine Arts<br> St. Petersburg</b><br> St. Petersburg, FL <br><br> On View until 6/15/2022`,
         icon: 'standard:brand'
         }, 
         {location: {
             Latitude: '29.435893',
             Longitude: '-98.482781',
         }, 
-        title: 'Untitled (Black and White Variation on "Pochade") - Stuart Davis',
-        description: `<b>San Antonio Art Museum</b><br> San Antonio, TX <br><br> On View: 1/15/2021 to 1/15/2022`,
+        title: `'Untitled (Black and White Variation on "Pochade")' by Stuart Davis`,
+        description: `<b>San Antonio Art Museum</b><br> San Antonio, TX <br><br> On View until 1/15/2022`,
         icon: 'standard:brand'
         }, 
         {location: {
             Latitude: '40.499538',
             Longitude: '-74.446489',
         }, 
-        title: "Lapsang - Jack Whitten",
-        description: `<b>Zimmerli Art Museum</b><br> New Brunswick, NJ <br> <br> On View: 1/5/2021 to 3/31/2022`,
+        title: "'Lapsang' by Jack Whitten",
+        description: `<b>Zimmerli Art Museum</b><br> New Brunswick, NJ <br> <br> On View until 3/31/2022`,
         icon: 'standard:brand'
         }
     ]
